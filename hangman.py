2fe18258f08b6e55e8fd17701db2e502ec4d4510
Shaginyan Artur <arturexp@gmail.com>
2020-08-26 01:02:50 +0700

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -4,6 +4,6 @@ print("H A N G M A N")
 
 word_list = ['python', 'java', 'kotlin', 'javascript']
 word = random.choice(word_list)
-word_input = input("Guess the word: ")
+word_input = input("Guess the word " + word[:3] + "-" * (len(word) - 3) + ": ")
 
 print("You survived!" if word == word_input else "You are hanged!")
