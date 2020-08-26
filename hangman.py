345039408d80238450941de0c5f24a3921c28464
Shaginyan Artur <arturexp@gmail.com>
2020-08-27 02:05:49 +0700

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -4,6 +4,29 @@ print("H A N G M A N")
 
 word_list = ['python', 'java', 'kotlin', 'javascript']
 word = random.choice(word_list)
-word_input = input("Guess the word " + word[:3] + "-" * (len(word) - 3) + ": ")
 
-print("You survived!" if word == word_input else "You are hanged!")
+letters_list = ["-"] * len(word)
+
+for i in range(8):
+
+	# if "-" not in letters_list:
+		# break
+	print("")
+	for let in letters_list:
+		print(let, end="")
+
+	user_guess = input("\nInput a letter: ")
+
+	if user_guess not in word:
+		print("No such letter in the word")
+	else:
+		temp_list = []
+		for m in range(len(word)):
+			if word[m] == user_guess:
+				temp_list.append(m)
+		for x in range(len(temp_list)):
+			letters_list[temp_list[x]] = user_guess
+
+print("""\nThanks for playing!
+We'll see how well you did in the next stage""")
+
