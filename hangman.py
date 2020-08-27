bc02a49f9693ed773d033fed278acc10bd342d2f
Shaginyan Artur <arturexp@gmail.com>
2020-08-28 00:23:40 +0700

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -3,23 +3,33 @@ import random
 print("H A N G M A N")
 
 word_list = ['python', 'java', 'kotlin', 'javascript']
-word = random.choice(word_list)
+word = random.choice(word_list)  
 
 letters_list = ["-"] * len(word)
+lives = 8
+guessed_letters = set()
+while lives != 0:
 
-for i in range(8):
-
-	# if "-" not in letters_list:
-		# break
 	print("")
-	for let in letters_list:
+	for let in letters_list:  
 		print(let, end="")
+
+	if "-" not in letters_list:
+		print("""\nYou guessed the word!
+You survived!""")
+		break
 
 	user_guess = input("\nInput a letter: ")
 
 	if user_guess not in word:
 		print("No such letter in the word")
+		lives -= 1
 	else:
+
+		if user_guess in guessed_letters:
+			print("No improvements")
+			lives -= 1
+
 		temp_list = []
 		for m in range(len(word)):
 			if word[m] == user_guess:
@@ -27,6 +37,9 @@ for i in range(8):
 		for x in range(len(temp_list)):
 			letters_list[temp_list[x]] = user_guess
 
-print("""\nThanks for playing!
-We'll see how well you did in the next stage""")
+		guessed_letters.add(user_guess)
+
+if lives == 0:
+	print("You are hanged!")
+
 
